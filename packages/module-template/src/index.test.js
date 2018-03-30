254aa596491fb3ee8d87d16eab01974db094dc9e
Jared Anderson <jared@tuxsudo.com>
2018-03-29 20:13:09 -0600

    configure with bolt

--- packages/module-template/src/index.test.js
+++ packages/module-template/src/index.test.js
@@ -1,5 +1,13 @@
-import isTrue from "./index";
+import React from "react";
+import isTrue, { Component } from "./index";
+import renderer from "react-test-renderer";
 
 test("index", () => {
   expect(isTrue()).toBe(true);
+});
+
+test("jsx", () => {
+  const component = renderer.create(<Component />);
+  const tree = component.toJSON();
+  expect(tree).toEqual({ type: "h1", props: {}, children: ["hi"] });
 });
---
+++ packages/module-template/src/story.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Component } from "./index";
+
+storiesOf("Module", module).add("example", () => <Component />);
